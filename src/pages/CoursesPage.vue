4ba2b7595be2d4bb7e04a1002999d8870d18c2aa
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2026-02-19 23:04:52 +0800

    Add fast loading in\ course page

--- src/pages/CoursesPage.vue
+++ src/pages/CoursesPage.vue
@@ -1,21 +1,28 @@
 <script>
 import api from '../api.js';
-import { ref, onBeforeMount } from 'vue';
+import { ref, onMounted } from 'vue';
 import CourseComponent from '../components/CourseComponent.vue';
 
 export default {
   components: { CourseComponent },
   setup() {
     const courses = ref([]);
+    const loading = ref(true);
+    const error = ref(null);
 
-    onBeforeMount(async () => {
-      const { data } = await api.get('https://coursebookingapi.onrender.com/courses');
-      console.log(data);
-      courses.value = data;
+    onMounted(async () => {
+      try {
+          const { data } = await api.get('https://coursebookingapi.onrender.com/courses');
+          courses.value = data;
+      } catch (err) {
+          console.error(err);
+          error.value = "Failed to load courses.";
+      } finally {
+          loading.value = false;
+      }
     });
 
-
-    return { courses };
+    return { courses, loading, error };
   }
 }
 </script>
@@ -23,11 +30,20 @@ export default {
 <template>
   <div class="container-fluid">
     <div class="row mt-3">
-            <div class="col my-3">
-                <h1 class="text-center text-dark py-1">Courses</h1>
-            </div>
-        </div>
-    <div class="row p-3 m-3">
+      <div class="col my-3">
+        <h1 class="text-center text-dark py-1">Courses</h1>
+      </div>
+    </div>
+
+    <div v-if="loading" class="text-center py-5">
+      <h4>Loading courses...</h4>
+    </div>
+
+    <div v-else-if="error" class="text-center text-danger py-5">
+      <h4>{{ error }}</h4>
+    </div>
+
+    <div v-else class="row p-3 m-3">
       <CourseComponent
         v-for="course in courses"
         :key="course._id"
@@ -36,3 +52,4 @@ export default {
     </div>
   </div>
 </template>
+
